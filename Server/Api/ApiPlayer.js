928eccc24468b361ad4af6a3f97c2bb9f6eac0ba
HenryT99 <hxt12021999@@gmail.com>
2020-10-05 10:33:26 +0700

    Fix Player API

--- Server/Api/ApiPlayer.js
+++ Server/Api/ApiPlayer.js
@@ -2,51 +2,72 @@
 //Get Players
 //Get Players on Team done
 //Get Player on ID done
-//Add Player
+//Add Player done
 //Update Player
 
 const router = require('express').Router()
 const Player = require('../models/Player.Schema')
 const Team = require('../models/Team.Schema')
-const { route } = require('./ApiLeague')
+const League = require('../models/League.Schema')
 
-router.post('/id', (req, res) =>{
-    Player.findById(req.body.IDPlayer, (err, player)=>{
-        if(!player) return res.json({ message:'Invalid'})
-        return res.json({message:'Valid'}, player)
+router.post('/id', (req, res) =>{//Find Players with ID
+    // Player.findById(req.body.IDPlayer, (err, player)=>{
+    //     if(!player) return res.json({ message:'Invalid'})
+    //     return res.json({message:'Valid'}, player)
+    // })
+    League.findById(req.body.IDLeague, (err,league)=>{
+        if(!league) return res.json({ message:'Invalid'})
+
+        // const teamIdx = league.teams.find(team=>team.ID === req.body.IDTeam)
+        league.teams.forEach(team=>{
+            team.forEach(player=>{
+                if (player.ID = req.body.ID === req.body.IDPlayer) 
+                return  res.json({message:'Found'}, player)
+            })
+        })
+        return res.json({message:'Not found'})
     })
 })
 
-router.post('/id_team', (req, res)=>{
-    Team.findById(req.body.IDTeam).populate('players')
-    .exec((err, team)=>{
-        if(err) return res.json({ message: 'Error' })
-        return res.json({message:'Valid'}, team)
+router.post('/id_team', (req, res)=>{//Find Player with ID Team
+    League.findById(req.body.IDLeague, (err,league)=>{
+        if(!league) return res.json({ message:'Invalid'})
+        
+        const team = league.teams.find(team => team.ID === req.body.IDTeam)
+        const player = team.players.find(player => player.ID===req.body.IDPlayer)
+        return res.json({ message:'Valid'}, player)
     })
+    // Team.findById(req.body.IDTeam).populate('players')
+    // .exec((err, team)=>{
+    //     if(err) return res.json({ message: 'Error' })
+    //     return res.json({message:'Valid'}, team)
+    // })
 })
 
-router.post('/add', (req, res)=>{
+router.post('/add', (req, res)=>{//Add Player
     try{
-        Team.findById(req.body.IDTeam, (err, team)=>{
-            const newPlayer = new Player({
+        League.findById(req.body.IDLeague, (err, league)=>{
+            const teamIdx = league.teams.findIndex(team=>team.ID === req.body.IDTeam)
+            const player = {
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
                 dateOfBirth: req.body.dateOfBirth,
                 typePlayer: req.body.typePlayer,
+                numberShirt: req.body.numberShirt,
+                goals: 0
+            }
+            league.teams[teamIdx].push(player)
+            league.save()
+            .then(() =>{
+                return res.json({ message:'Success'})
             })
-            newPlayer.save()
-            team.players.push(newPlayer)
-                team.save()
-                .then(()=>{
-                    return res.json({message:'Success'})
-                })
-        });
-        
+        })
     }
     catch(err){
         return res.json({ message:err.message})
     }
     
 })
+
 
 module.exports = router
